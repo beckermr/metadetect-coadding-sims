a18e6adfc04d08bea4c7da59fd85a2159eb2a57f
Matthew R Becker <becker.mr@gmail.com>
2019-11-21 11:28:24 -0600

    ENH added code to run stuff on condor

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name="coadd_mdetsims",
     version="0.1",
     packages=find_packages(),
-    scripts=['scripts/coadd-mdetsims-run-sims']
+    scripts=[
+        'scripts/coadd-mdetsims-run-sims',
+        'scripts/coadd-mdetsims-setup-condor-jobs']
 )
