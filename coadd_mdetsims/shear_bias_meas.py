476408c003f510379e8dcce759bdf0a760b7c0df
Matthew R Becker <becker.mr@gmail.com>
2019-11-22 15:26:17 -0600

    clean up a bit

--- coadd_mdetsims/shear_bias_meas.py
+++ coadd_mdetsims/shear_bias_meas.py
@@ -34,64 +34,64 @@ def _meas_m_c_cancel(dp, dm, inds, swap12, step, g_true):
     return g1 / R11 / g_true - 1, g2 / R22
 
 
-# def estimate_m_and_c(
-#         data_p, data_m, g_true, swap12=False, step=0.01,
-#         rng=None, n_boot=500, verbose=True):
-#     """Estimate m and c w/ errors via boostrapping.
-#
-#     Parameters
-#     ----------
-#     data_p : array-like
-#         Array of simulation data estimated from `measure_shear_metadetect`
-#         with a +g_true true shear.
-#     data_m : array-like
-#         Array of simulation data estimated from `measure_shear_metadetect`
-#         with a -g_true true shear.
-#     g_true : float
-#         The true value of the shear on the 1-axis in the simulation. The other
-#         axis is assumd to havea true value of zero.
-#     swap12 : bool, optional
-#         If True, swap the roles of the 1- and 2-axes in the computation.
-#     step : float, optional
-#         The step used in metadetect for estimating the response. Default is
-#         0.01.
-#     rng : np.random.RandomState, int or None
-#         An RNG to use for drawing the objects. If an int or None is passed,
-#         the input is used to initialize a new `np.random.RandomState` object.
-#     n_boot : int, optional
-#         The number of iterations used for estimating the bootstrap errors.
-#     verbose : bool, optional
-#         If True, show a progress bar. Otherwise show nothing.
-#
-#     Returns
-#     -------
-#     m : float
-#         Estimate of the multiplicative bias.
-#     merr : float
-#         Estimat of the 1-sigma standard error in `m`.
-#     c : float
-#         Estimate of the additive bias.
-#     cerr : float
-#         Estimate of the 1-sigma standard error in `c`.
-#     """
-#     n_data = data_p.shape[0]
-#     ms = []
-#     cs = []
-#     rng = (rng if isinstance(rng, np.random.RandomState)
-#            else np.random.RandomState(seed=rng))
-#     for _ in tqdm.trange(n_boot, total=n_boot, disable=not verbose):
-#         inds = rng.choice(n_data, replace=True, size=n_data)
-#         _m, _c = _meas_m_c_cancel(data_p, data_m, inds, swap12, step, g_true)
-#         ms.append(_m)
-#         cs.append(_c)
-#
-#     m, c = _meas_m_c_cancel(
-#         data_p, data_m, np.arange(n_data), swap12, step, g_true)
-#
-#     return m, np.std(ms), c, np.std(cs)
-
-
 def estimate_m_and_c(
+        data_p, data_m, g_true, swap12=False, step=0.01,
+        rng=None, n_boot=500, verbose=True):
+    """Estimate m and c w/ errors via boostrapping.
+
+    Parameters
+    ----------
+    data_p : array-like
+        Array of simulation data estimated from `measure_shear_metadetect`
+        with a +g_true true shear.
+    data_m : array-like
+        Array of simulation data estimated from `measure_shear_metadetect`
+        with a -g_true true shear.
+    g_true : float
+        The true value of the shear on the 1-axis in the simulation. The other
+        axis is assumd to havea true value of zero.
+    swap12 : bool, optional
+        If True, swap the roles of the 1- and 2-axes in the computation.
+    step : float, optional
+        The step used in metadetect for estimating the response. Default is
+        0.01.
+    rng : np.random.RandomState, int or None
+        An RNG to use for drawing the objects. If an int or None is passed,
+        the input is used to initialize a new `np.random.RandomState` object.
+    n_boot : int, optional
+        The number of iterations used for estimating the bootstrap errors.
+    verbose : bool, optional
+        If True, show a progress bar. Otherwise show nothing.
+
+    Returns
+    -------
+    m : float
+        Estimate of the multiplicative bias.
+    merr : float
+        Estimat of the 1-sigma standard error in `m`.
+    c : float
+        Estimate of the additive bias.
+    cerr : float
+        Estimate of the 1-sigma standard error in `c`.
+    """
+    n_data = data_p.shape[0]
+    ms = []
+    cs = []
+    rng = (rng if isinstance(rng, np.random.RandomState)
+           else np.random.RandomState(seed=rng))
+    for _ in tqdm.trange(n_boot, total=n_boot, leave=False, disable=not verbose):
+        inds = rng.choice(n_data, replace=True, size=n_data)
+        _m, _c = _meas_m_c_cancel(data_p, data_m, inds, swap12, step, g_true)
+        ms.append(_m)
+        cs.append(_c)
+
+    m, c = _meas_m_c_cancel(
+        data_p, data_m, np.arange(n_data), swap12, step, g_true)
+
+    return m, np.std(ms), c, np.std(cs)
+
+
+def estimate_m_and_c_patch_avg(
         data_p, data_m, g_true, swap12=False, step=0.01,
         rng=None, n_boot=500, verbose=True):
     """Estimate m and c from paired lensing simulations.
@@ -132,15 +132,26 @@ def estimate_m_and_c(
         Estimate of the 1-sigma standard error in `c`.
     """
 
-    g1p = data_p['g1'].copy()
-    R11p = (data_p['g1p'] - data_p['g1m']) / 2 / step * g_true
-    g2p = data_p['g2'].copy()
-    R22p = (data_p['g2p'] - data_p['g2m']) / 2 / step
+    if swap12:
+        g1p = data_p['g2'].copy()
+        R11p = (data_p['g2p'] - data_p['g2m']) / 2 / step * g_true
+        g2p = data_p['g1'].copy()
+        R22p = (data_p['g1p'] - data_p['g1m']) / 2 / step
 
-    g1m = data_m['g1'].copy()
-    R11m = (data_m['g1p'] - data_m['g1m']) / 2 / step * g_true
-    g2m = data_m['g2'].copy()
-    R22m = (data_m['g2p'] - data_m['g2m']) / 2 / step
+        g1m = data_m['g2'].copy()
+        R11m = (data_m['g2p'] - data_m['g2m']) / 2 / step * g_true
+        g2m = data_m['g1'].copy()
+        R22m = (data_m['g1p'] - data_m['g1m']) / 2 / step
+    else:
+        g1p = data_p['g1'].copy()
+        R11p = (data_p['g1p'] - data_p['g1m']) / 2 / step * g_true
+        g2p = data_p['g2'].copy()
+        R22p = (data_p['g2p'] - data_p['g2m']) / 2 / step
+
+        g1m = data_m['g1'].copy()
+        R11m = (data_m['g1p'] - data_m['g1m']) / 2 / step * g_true
+        g2m = data_m['g2'].copy()
+        R22m = (data_m['g2p'] - data_m['g2m']) / 2 / step
 
     wgts = data_p['n1'].copy()
     wgts /= np.sum(wgts)
@@ -151,10 +162,11 @@ def estimate_m_and_c(
     x2 = (R22p + R22m) / 2
     y2 = (g2p + g2m) / 2
 
-    rng = np.random.RandomState(seed=100)
+    rng = (rng if isinstance(rng, np.random.RandomState)
+           else np.random.RandomState(seed=rng))
     mvals = []
     cvals = []
-    for _ in tqdm.trange(500, leave=False):
+    for _ in tqdm.trange(n_boot, total=n_boot, leave=False, disable=not verbose):
         ind = rng.choice(len(y1), replace=True, size=len(y1))
         _wgts = wgts[ind].copy()
         _wgts /= np.sum(_wgts)
